cd7b19f44ef133874f7f525332085e13742e797a
chris <christian.buongarzoni@gmail.com>
2024-08-30 19:01:33 -0300

    refactor: rename

--- src/main/kotlin/scoring/CalculateScoring.kt
+++ src/main/kotlin/scoring/CalculateScoring.kt
@@ -2,27 +2,29 @@ package org.buongarzoni.scoring
 
 class CalculateScoring {
     fun execute(user: User, anotherUser: User): Accuracy {
-        if (user haveSameIdAs anotherUser) return Accuracy.HIGH
-        if (user haveSameEmail anotherUser) return Accuracy.HIGH
-        if (user haveSameEmailName anotherUser && user haveSimilarFullName anotherUser) {
-            return if (user haveSimilarLocation anotherUser) {
+        if (user isIdEqualsTo anotherUser) return Accuracy.HIGH
+        if (user isEmailEqualsTo anotherUser) return Accuracy.HIGH
+        if (user isEmailNameEqualsTo anotherUser && user isFullNameSimilarTo anotherUser) {
+            return if (user isLocationSimilarTo anotherUser) {
                 Accuracy.HIGH
             } else {
                 Accuracy.MEDIUM
             }
         }
-        if (user haveSimilarFullName anotherUser && user haveSimilarLocation anotherUser) return Accuracy.MEDIUM
+        if (user isFullNameSimilarTo anotherUser && user isLocationSimilarTo anotherUser) return Accuracy.MEDIUM
 
         return Accuracy.LOW
     }
 
-    private infix fun User.haveSameIdAs(another: User) = id == another.id
-    private infix fun User.haveSameEmail(another: User) = email == another.email
-    private infix fun User.haveSameEmailName(another: User) = email.substringBefore('@') == another.email.substringBefore('@')
-    private infix fun User.haveSimilarLocation(another: User) = haveSameZipCodeAs(another) || haveSameAddressAs(another)
+    private infix fun User.isIdEqualsTo(another: User) = id == another.id
+    private infix fun User.isEmailEqualsTo(another: User) = email == another.email
+    private infix fun User.isEmailNameEqualsTo(another: User) = email.substringBefore('@') == another.email.substringBefore('@')
+
+    private infix fun User.isLocationSimilarTo(another: User) = haveSameZipCodeAs(another) || haveSameAddressAs(another)
     private infix fun User.haveSameAddressAs(another: User) = address.contains(another.address) || another.address.contains(address)
     private infix fun User.haveSameZipCodeAs(another: User) = zipCode == another.zipCode
-    private infix fun User.haveSimilarFullName(another: User) = similarLastNameTo(another) && similarNameTo(another)
-    private infix fun User.similarNameTo(another: User) = name.contains(another.name) || another.name.contains(name)
-    private infix fun User.similarLastNameTo(another: User) = lastName.contains(another.lastName) || another.name.contains(lastName)
+
+    private infix fun User.isFullNameSimilarTo(another: User) = haveSimilarLastNameAs(another) && haveSimilarNameAs(another)
+    private infix fun User.haveSimilarNameAs(another: User) = name.contains(another.name) || another.name.contains(name)
+    private infix fun User.haveSimilarLastNameAs(another: User) = lastName.contains(another.lastName) || another.name.contains(lastName)
 }
